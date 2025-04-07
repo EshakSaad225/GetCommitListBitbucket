a6c3468a699ed82a5a86fdfa305ff649f15fcc79
EshakSaad225 <eshak.saad225@gmail.com>
2025-04-08 00:54:15 +0200

    Handling  async / await

--- app.js
+++ app.js
@@ -4,65 +4,67 @@ const username = '';
 const appPassword = '';
 const workspace = '';
 const repoName = '';
-const targetDate = new Date("2025-03-01");
+const targetDate = new Date("2024-10-01");
 // const targetDate = null;
 let filteredCommits;
-let flag = false;
+let counter = 0;
+let x = 0;
+let pageLength = 0;
 
 let url = `https://api.bitbucket.org/2.0/repositories/${workspace}/${repoName}/commits`;
 
-fetch(url, {
-    method: 'GET',
-    headers: {
-        'Authorization': 'Basic ' + Buffer.from(`${username}:${appPassword}`).toString('base64')
-    }
-})
-    .then(res => {
-        if (!res.ok) {
-            console.log(`HTTP error! status: ${res.status}`);
-            return null;
-        }
-        return res.json();
-    })
-    .then(data => {
-        do {
-            if (data.values) {
-                if (targetDate) {
-                    filteredCommits = data.values.filter(commit => {
-                        const commitDate = new Date(commit.date);
-                        return commitDate >= targetDate;
-                    });
-                }
-                else {
-                    filteredCommits = data.values;
-                }
-                console.log(`Total Commits is : ${filteredCommits.length}\n`);
-                filteredCommits.forEach((commit, i) => {
-                    console.log(`Commit => ${i + 1}`);
-                    console.log('Message :', commit.message);
-                    console.log('Hash :', commit.hash);
-                    console.log('User :', commit.author.user.display_name);
-                    const [date, time] = commit.date.split('T');
-                    console.log('Date :', date);
-                    console.log('Time :', time.split('+')[0]);
-                    console.log("\n#################\n")
-                });
-
-                url = data.next || null;
-
-                if (url) {
-                    flag = true;
-                }
-                else {
-                    flag = false;
-                }
-            } else {
-                console.log('No commits found or error occurred:', data);
+async function getCommits() {
+    while (url) {
+        // console.log("Fetching:", url);
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from(`${username}:${appPassword}`).toString('base64')
             }
-        } while (flag)
-    }
-    )
-    .catch(err => {
-        console.error('Error:', err); 
-    });
+        });
 
+        const data = await res.json();
+
+        if (data.values) {
+            if (targetDate) {
+                filteredCommits = data.values.filter(commit => {
+                    const commitDate = new Date(commit.date);
+                    return commitDate >= targetDate;
+                });
+            }
+            else {
+                filteredCommits = data.values;
+            }
+
+            if (filteredCommits.length === 0) {
+                url = null;
+                break;
+            }
+
+            if (counter === 0) {
+                pageLength = filteredCommits.length;
+            }
+
+            x = pageLength * counter;
+            console.log(`Commits ${x + 1} => ${x + filteredCommits.length}\n`);
+            filteredCommits.forEach((commit, i) => {
+                console.log(`Commit => ${i + 1 + x}`);
+                console.log('Message :', commit.message);
+                console.log('Hash :', commit.hash);
+                console.log('User :', commit.author?.user?.display_name || commit.author?.raw || "Unknown User");
+                const [date, time] = commit.date.split('T');
+                console.log('Date :', date);
+                console.log('Time :', time.split('+')[0]);
+                console.log("\n#################\n")
+            });
+
+        } else {
+            console.log('No commits found or error occurred:', data);
+        }
+
+        url = data.next;
+        counter++;
+    }
+}
+
+getCommits();
