0f66996b47ffd2ed51472e122f77851da4f436a1
EshakSaad225 <eshak.saad225@gmail.com>
2025-04-07 21:33:22 +0200

    Handel Date Filtter & Display All Data

--- app.js
+++ app.js
@@ -2,42 +2,67 @@ import fetch from 'node-fetch';
 
 const username = '';
 const appPassword = '';
-const workspace = 'pentabdev';
+const workspace = '';
 const repoName = '';
+const targetDate = new Date("2025-03-01");
+// const targetDate = null;
+let filteredCommits;
+let flag = false;
 
-const url = `https://api.bitbucket.org/2.0/repositories/${workspace}/${repoName}/commits`;
+let url = `https://api.bitbucket.org/2.0/repositories/${workspace}/${repoName}/commits`;
 
 fetch(url, {
-  method: 'GET',
-  headers: {
-    'Authorization': 'Basic ' + Buffer.from(`${username}:${appPassword}`).toString('base64')
-  }
+    method: 'GET',
+    headers: {
+        'Authorization': 'Basic ' + Buffer.from(`${username}:${appPassword}`).toString('base64')
+    }
 })
-  .then(res => {
-    if (!res.ok) {
-      console.log(`HTTP error! status: ${res.status}`);
-      return null ;
-    }
-    return res.json(); 
-  })
-  .then(data => {
-    if (data.values) {
-      console.log(`Total Commits is : ${data.values.length}\n`);
-      data.values.forEach((commit, i) => {
-        console.log(`Commit => ${i + 1}`);
-        console.log('Message :', commit.message);
-        console.log('Hash :', commit.hash);
-        console.log('User :', commit.author.user.display_name);
-        const [date, time] = commit.date.split('T');
-        console.log('Date :', date);
-        console.log('Time :', time.split('+')[0]); 
-        console.log("\n#################\n")
+    .then(res => {
+        if (!res.ok) {
+            console.log(`HTTP error! status: ${res.status}`);
+            return null;
+        }
+        return res.json();
+    })
+    .then(data => {
+        do {
+            if (data.values) {
+                if (targetDate) {
+                    filteredCommits = data.values.filter(commit => {
+                        const commitDate = new Date(commit.date);
+                        return commitDate >= targetDate;
+                    });
+                }
+                else {
+                    filteredCommits = data.values;
+                }
+                console.log(`Total Commits is : ${filteredCommits.length}\n`);
+                filteredCommits.forEach((commit, i) => {
+                    console.log(`Commit => ${i + 1}`);
+                    console.log('Message :', commit.message);
+                    console.log('Hash :', commit.hash);
+                    console.log('User :', commit.author.user.display_name);
+                    const [date, time] = commit.date.split('T');
+                    console.log('Date :', date);
+                    console.log('Time :', time.split('+')[0]);
+                    console.log("\n#################\n")
+                });
 
-      });
-    } else {
-      console.log('No commits found or error occurred:', data);
+                url = data.next || null;
+
+                if (url) {
+                    flag = true;
+                }
+                else {
+                    flag = false;
+                }
+            } else {
+                console.log('No commits found or error occurred:', data);
+            }
+        } while (flag)
     }
-  })
-  .catch(err => {
-    console.error('Error:', err); // عرض الخطأ إذا كان موجودًا.
-  });
+    )
+    .catch(err => {
+        console.error('Error:', err); 
+    });
+
